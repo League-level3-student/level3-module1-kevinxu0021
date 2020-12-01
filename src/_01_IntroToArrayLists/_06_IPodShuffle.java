354d65c74c35b2bc3293366a90b3e96a010ef44c
kevinxu0021 <32040253+kevinxu0021@users.noreply.github.com>
2020-11-30 17:00:28 -0800

    iPod Shuffle

--- src/_01_IntroToArrayLists/_06_IPodShuffle.java
+++ src/_01_IntroToArrayLists/_06_IPodShuffle.java
@@ -1,17 +1,29 @@
 package _01_IntroToArrayLists;
 
+import java.awt.event.ActionEvent;
+
+import java.awt.event.ActionListener;
 import java.util.ArrayList;
 import java.util.Random;
 
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 //Copyright The League of Amazing Programmers, 2015
 
-public class _06_IPodShuffle{
+public class _06_IPodShuffle implements ActionListener {
+	JFrame frame = new JFrame();
+	JPanel panel = new JPanel();
+	JButton button = new JButton("Suprise Me!");
+	ArrayList<String> songs = new ArrayList<String>();
+	Song player;
+	Random random = new Random();
+	int a = 0;
+
 	public _06_IPodShuffle() {
 		// 1. Use the Song class the play the demo.mp3 file.
-				Song s = new Song("demo.mp3");
-				s.play();
-				
+
 		/**
 		 * 2. Congratulations on completing the sound check! * Now we want to make an
 		 * iPod Shuffle that plays random music. * Create an ArrayList of Songs and a
@@ -19,10 +31,47 @@ public class _06_IPodShuffle{
 		 * you're really cool, you can stop all the songs, before playing a new one on
 		 * subsequent button clicks.
 		 */
-		
+
 	}
-	
+
 	public static void main(String[] args) {
-		new _06_IPodShuffle();
+
+		_06_IPodShuffle a = new _06_IPodShuffle();
+		a.addSongs();
+		a.run();
+
+	}
+
+	public void run() {
+		frame.add(panel);
+		panel.add(button);
+		button.addActionListener(this);
+		frame.setVisible(true);
+		frame.setResizable(false);
+		frame.pack();
+	}
+
+	public void addSongs() {
+		for (int i = 1; i < 28; i++) {
+			songs.add(" (" + i + ").mp3");
+		}
+
+	}
+
+	public void play() {
+
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		// TODO Auto-generated method stub
+
+		if (a > 0) {
+			player.stop();
+		}
+		player = new Song(songs.get(random.nextInt(songs.size())));
+		player.play();
+		a++;
+
 	}
 }
