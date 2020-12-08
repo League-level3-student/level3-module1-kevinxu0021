9cb033700677973bb94f246aba24951108e06f37
kevinxu0021 <32040253+kevinxu0021@users.noreply.github.com>
2020-12-07 16:59:40 -0800

    Integer Stack

--- src/_02_Integer_Stack/IntegerStack.java
+++ src/_02_Integer_Stack/IntegerStack.java
@@ -1,50 +1,59 @@
 package _02_Integer_Stack;
 
 public class IntegerStack {
-	//1. create a private array of integers
-	
-	//2. complete the constructor by initializing the member array
-	//   to a new array of length 0. This prevents null pointer exceptions.
+	// 1. create a private array of integers
+	private int[] a;
+
+	// 2. complete the constructor by initializing the member array
+	// to a new array of length 0. This prevents null pointer exceptions.
 	public IntegerStack() {
+		a = new int[0];
 	}
-	
-	//3. Complete the steps in the push method.
+
+	// 3. Complete the steps in the push method.
 	public void push(int v) {
-		//A. create a new array that is one element larger than the member array
-		
-		//B. set the last element of the new array equal to the integer passed into the method
-		
-		//C. iterate through the member array and copy all the elements into the new array
-		
-		//D. set the member array equal to the new array.
-		
+		// A. create a new array that is one element larger than the member array
+		int[] b = new int[a.length + 1];
+		// B. set the last element of the new array equal to the integer passed into the
+		// method
+		b[b.length-1] = v;
+		// C. iterate through the member array and copy all the elements into the new
+		// array
+		for (int i = 0; i < a.length; i++) {
+			b[i] = a[i];
+		}
+		// D. set the member array equal to the new array.
+		a = b;
 	}
-	
-	//4. Complete the steps in the pop method.
+
+	// 4. Complete the steps in the pop method.
 	public int pop() {
-		//A. create an integer variable and initialize it to the
-		//   last element of the member array.
-		
-		//B. create a new array that is one element smaller than the member array
-		
-		//C. iterate through the new array and copy every element from the
-		//   member array to the new array
-		
-		//D. set the member array equal to the new array
-		
-		//E. return the variable you created in step A
-		return 0;
+		// A. create an integer variable and initialize it to the
+		// last element of the member array.
+		int c = a[a.length-1];
+		// B. create a new array that is one element smaller than the member array
+		int[] d = new int[a.length - 1];
+		// C. iterate through the new array and copy every element from the
+		// member array to the new array
+		for (int i = 0; i < d.length; i++) {
+			d[i] = a[i];
+		}
+		// D. set the member array equal to the new array
+		a = d;
+		// E. return the variable you created in step A
+		return c;
 	}
-	
-	//5. Complete the clear method to set the
-	//   member array to a new array of length 0
+
+	// 5. Complete the clear method to set the
+	// member array to a new array of length 0
 	public void clear() {
+		a = new int[0];
 		
 	}
-	
-	//6. Complete the size method to return 
-	//   the length of the member array
+
+	// 6. Complete the size method to return
+	// the length of the member array
 	public int size() {
-		return 0;
+		return a.length;
 	}
 }
