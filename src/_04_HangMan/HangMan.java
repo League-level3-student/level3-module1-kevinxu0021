40053993e51b4fec94efd09d8276fbb0a09cffec
kevinxu0021 <32040253+kevinxu0021@users.noreply.github.com>
2021-01-04 17:04:11 -0800

    Hangman

--- src/_04_HangMan/HangMan.java
+++ src/_04_HangMan/HangMan.java
@@ -1,7 +1,23 @@
 package _04_HangMan;
 
-public class HangMan{
+import javax.swing.JOptionPane;
+
+public class HangMan {
+	String userInput;
+	int intUserInput;
+	HangMan(){
+		
+	}
+	public static void main(String[] args) {
+		HangMan hangMan = new HangMan();
+		hangMan.run();
+
+
+	}
 	
-	
-	
+	void run() {
+		userInput = JOptionPane.showInputDialog("Enter a number from 1-266");
+		intUserInput = Integer.parseInt(userInput);
+	}
+
 }
